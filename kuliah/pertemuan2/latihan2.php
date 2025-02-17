47e75b650dffb8e7f955df4f9671c7e25cd0bce9
NugrahaaPutraaa <pnugraha328@gmail.com>
2025-02-17 21:37:48 +0700

    tugas 1 selesai

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php 
+    $nama = "Nugraha Putra Juliansyah";
+    $email = "Pnugraha328@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, nama saya $nama, saya adalah $pekerjaan di $jurusan $universitas</p>";
+
+?>
